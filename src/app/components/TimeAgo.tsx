432fa3bd768e8bc4373596fbe2479582bc21421a
PADRIAN101 <janadrian11224@gmail.com>
2024-11-30 00:20:38 -0400

    initial commit

--- src/app/components/TimeAgo.tsx
+++ src/app/components/TimeAgo.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
 import ReactTimeAgo from 'react-timeago';
 
 export default function TimeAgo({createdAt}:{createdAt:string}) {
