6e8b926376be5cd59819aca4c4d5c52f1cf810e2
PADRIAN101 <janadrian11224@gmail.com>
2024-12-01 23:07:35 -0400

    initial commit dec 1

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig = {
             {
                 protocol: 'https',
                 hostname: 'padrian-community-board.s3.amazonaws.com',
+                unoptimized: true
             },
         ],
         domains: ['example.com'],
